3491b47af71ef938708423280ea7ee6d9f248c86
Fernando Ayats <ayatsfer@gmail.com>
2023-03-26 12:38:35 +0200

    no jupyter

---
+++ parse_txt.py
@@ -0,0 +1,17 @@
+import sys
+from typing import Dict
+import textract
+import openai
+from pathlib import Path
+
+pdfs_dir = (Path() / "priv").resolve()
+
+pdfs = [file for file in pdfs_dir.glob("**/*.pdf")]
+print(pdfs)
+
+pdfs_text: Dict[Path, str] = {f: textract.process(str(f), method="pdfminer").decode() for f in pdfs}
+
+for (f,content) in pdfs_text.items():
+    newpath = f.parent / f"{f.name}.txt"
+    with open(newpath, "w") as newfile:
+        newfile.write(content)
--- perSystem.nix
+++ perSystem.nix
@@ -1,20 +1,12 @@
 {pkgs, ...}: {
-  devShells.default = pkgs.mkShell rec {
-    myPython = pkgs.python3.withPackages (p:
-      with p; [
-        jupyter
-        openai
-      ]);
-    packages = [
-      myPython
-    ];
-    LD_LIBRARY_PATH = with pkgs;
-      lib.makeLibraryPath [
-        stdenv.cc.cc
+  devShells.default = with pkgs;
+    mkShell {
+      packages = [
+        python3
+        poetry
       ];
-    shellHook = ''
-      venv="$(cd $(dirname $(which python)); cd ..; pwd)"
-      ln -Tsf "$venv" .venv
-    '';
-  };
+      # LD_LIBRARY_PATH = lib.makeLibraryPath [
+      #   stdenv.cc.cc
+      # ];
+    };
 }
